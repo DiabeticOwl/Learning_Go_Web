42b765903a9b2d6b11fe9be5ae8b46cb0e499d3a
DiabeticOwl <jh.cruz91@gmail.com>
2022-06-06 02:23:09 -0400

    Serving files - Exercise #4.

---
+++ 02-servers/exercises/03-serving-files/04/main.go
@@ -0,0 +1,35 @@
+// # Serve the files in the "starting-files" folder
+
+// ## To get your images to serve, use
+
+// ``` Go
+//  func StripPrefix(prefix string, h Handler) Handler
+//  func FileServer(root FileSystem) Handler
+// ```
+
+// Constraint: you are not allowed to change the route being used for images in the template file
+package main
+
+import (
+	"html/template"
+	"log"
+	"net/http"
+)
+
+var tpl *template.Template
+
+func main() {
+	fs := http.FileServer(http.Dir("./starting-files/public"))
+	fs = http.StripPrefix("/resources", fs)
+
+	http.Handle("/resources/", fs)
+	http.HandleFunc("/", sTpl)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func sTpl(w http.ResponseWriter, r *http.Request) {
+	tpl = template.Must(template.ParseGlob("./starting-files/templates/*.gohtml"))
+
+	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+}
